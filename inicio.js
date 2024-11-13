38b5b11caf9f02762e0ab88a1a44caf95e9c42c7
masarepas <mariapaulabedoyap@gmail.com>
2024-11-12 21:19:02 -0500

    mi primer repositorio

---
+++ inicio.js
@@ -0,0 +1,16 @@
+function Administrativos() {
+    window.location = "admistra.html"
+    
+}
+
+
+function servicios() {
+    window.location = "servicios.html"
+    
+}
+
+
+function Productos() {
+    window.location = "productos.html"
+    
+}
